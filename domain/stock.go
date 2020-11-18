0b23af73292955e50b4dda17c2e41d59dea96b68
k <mol.tired@gmail.com>
2020-11-18 17:25:26 -0500

    add domain entities

---
+++ domain/stock.go
@@ -0,0 +1,6 @@
+package domain
+
+type Stock struct {
+	ID          int64
+	Description string
+}
---
+++ domain/store.go
@@ -0,0 +1,8 @@
+package domain
+
+type Store struct {
+	ID    int64
+	Owner int64
+	Stock int64
+	Name  string
+}
